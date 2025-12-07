d9396da2f99843069a3c8155159a9491808bb664
Arash Adeli Nik <arash.adeli20@gmail.com>
2025-12-07 18:03:52 +0330

    Update main.py

--- Project-01-Invoice-Automation/src/main.py
+++ Project-01-Invoice-Automation/src/main.py
@@ -1,1 +1,12 @@
+from utils import load_template
+from config import settings
 
+def generate_invoice():
+    print("Starting Invoice Automation System...")
+    template = load_template()
+    print(f"Template loaded: {template}")
+    print(f"Customer name: {settings['customer_name']}")
+    print("Invoice generation complete (mock).")
+
+if __name__ == "__main__":
+    generate_invoice()
